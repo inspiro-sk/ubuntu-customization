942f1408b2002c160150b249b43e70135d1133aa
Rastislav Stana <development@inspiro.sk>
2024-09-14 16:36:10 +0200

    Initial commit

---
+++ conky-customization.sh
@@ -0,0 +1,13 @@
+# download conky-config from www.pling.com/p/2033080
+
+sudo apt -y install conky-all jq curl moc
+
+# sudo nano /etc/environment
+# MUFFIN_NO_SHADOWS=1
+
+unzip ~/Downloads/conky-config.zip ~/Downloads/
+
+rsync -av ~/Downloads/conky-config/conky ~/.config
+rsync -av ~/Downloads/conky-config/autostart ~/.config
+
+# rm -rf ~/Downloads/conky-config
